b9e21b054dfc228395f6792bb4b41fa92f99f0a6
Mohd Adil <shaikhmdadil191@gmail.com>
2026-02-08 16:34:22 +0530

    added arcjet protection

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { signupController, loginController, logoutController } from "../controllers/auth.controller";
+import { arcjetProtection } from "../middlewares/arcjet.middleware";
 
 const router = Router();
-
+router.use(arcjetProtection); // Apply Arcjet protection to all auth routes
 // Signup route
 router.post("/signup", signupController);
 
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { completeUserProfile, getMe, updateUserProfile } from "../controllers/user.controller";
 import { protect } from "../middlewares/auth.middleware";
-
+import { arcjetProtection } from "../middlewares/arcjet.middleware";
 const router = Router();
-
+router.use(arcjetProtection) // Apply Arcjet protection to all user routes
 router.use(protect)
 router.get("/getMe",getMe)
 router.post("/complete-profile",completeUserProfile)
